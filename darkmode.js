3676e139cee20df68e7bba0fe41c9252cde35609
LucasDCoelho <jessicamaiaaraujo@gmail.com>
2023-02-27 11:11:57 -0300

    aula gleidson

--- darkmode.js
+++ darkmode.js
@@ -25,3 +25,62 @@ function ativarDarkmode() {
 
    alterarCores()
 }
+
+
+
+/////////////////ARRAY DE OBJETOS /////////////////////////
+
+let alunos = [
+    {
+        id: 1,
+        nome: "Ana Kelly",
+        email: "kelly@gmail.com",
+        telefone: "00 0000-0000",
+        cidade: "Fortaleza-CE"
+        
+    },
+    {
+        id: 2,
+        nome: "Andy Silva",
+        email: "kelly@gmail.com",
+        telefone: "00 0000-0000",
+        cidade: "Fortaleza-CE"
+        
+    },
+    {
+        id: 3,
+        nome: "Lane Gomes",
+        email: "kelly@gmail.com",
+        telefone: "00 0000-0000",
+        cidade: "Fortaleza-CE"
+        
+    },
+    {
+        id: 4,
+        nome: "Sara Camilo",
+        email: "kelly@gmail.com",
+        telefone: "00 0000-0000",
+        cidade: "Fortaleza-CE"
+        
+    },
+    {
+        id: 5,
+        nome: "Samantha Kuran",
+        email: "kelly@gmail.com",
+        telefone: "00 0000-0000",
+        cidade: "Fortaleza-CE"
+        
+    }
+]
+
+let tabelab = document.getElementById("tabela-body");
+tabelab.innerHTML = `<tr>
+<td>${alunos[4].nome}</td>
+<td>${alunos[4].email}</td>
+<td>${alunos[4].telefone}</td>
+<td>${alunos[4].cidade}</td>
+<td>
+    <button class="btn btn-warning">Editar</button>
+    <button class="btn btn-danger">Excluir</button>
+</td>
+</tr>`;
